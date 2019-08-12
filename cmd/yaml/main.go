a689462b86733acb4c15cd74d372d8b8ae5f8061
Vojtech Vitek <vojtech.vitek@gmail.com>
2019-08-12 21:54:06 +0200

    Support multiple input YAML documents separated by '---'

--- cmd/yaml/main.go
+++ cmd/yaml/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,6 +11,7 @@ import (
 	"github.com/VojtechVitek/yaml"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
+	yamlv3 "gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -23,91 +25,94 @@ func runCLI() error {
 		return errors.New("usage: yaml apply [files..]")
 	}
 
-	in, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		return errors.Wrap(err, "failed to read stdin")
-	}
+	dec := yamlv3.NewDecoder(os.Stdin)
+	enc := yamlv3.NewEncoder(os.Stdout)
+	enc.SetIndent(2)
 
-	doc, err := yaml.Parse(in)
-	if err != nil {
-		return errors.Wrap(err, "failed to unmarshal input")
-	}
-
-	switch os.Args[1] {
-	case "apply":
-		filenames := os.Args[2:]
-		transformations := make([]*yaml.Transformation, len(filenames))
-
-		var g errgroup.Group
-		for i, filename := range filenames {
-			i, filename := i, filename
-			g.Go(func() error {
-				b, err := ioutil.ReadFile(filename)
-				if err != nil {
-					return errors.Wrapf(err, "failed to read transformation %v", filename)
-				}
-
-				transformations[i], err = yaml.ParseTransformation(b)
-				if err != nil {
-					return errors.Wrapf(err, "failed to parse transformation %v", filename)
-				}
-
+	for { // For all YAML documents (they might be separated by '---')
+		var doc yamlv3.Node
+		if err := dec.Decode(&doc); err != nil {
+			if err == io.EOF { // Last document.
 				return nil
-			})
-		}
-		if err := g.Wait(); err != nil {
-			return err
+			}
+			return errors.Wrap(err, "failed to decode YAML document(s) from stdin")
 		}
 
-		for i, tf := range transformations {
-			ok, _ := tf.MustMatchAll(doc)
-			if !ok {
-				continue
+		switch os.Args[1] {
+		case "apply":
+			filenames := os.Args[2:]
+			transformations := make([]*yaml.Transformation, len(filenames))
+
+			var g errgroup.Group
+			for i, filename := range filenames {
+				i, filename := i, filename
+				g.Go(func() error {
+					b, err := ioutil.ReadFile(filename)
+					if err != nil {
+						return errors.Wrapf(err, "failed to read transformation %v", filename)
+					}
+
+					transformations[i], err = yaml.ParseTransformation(b)
+					if err != nil {
+						return errors.Wrapf(err, "failed to parse transformation %v", filename)
+					}
+
+					return nil
+				})
+			}
+			if err := g.Wait(); err != nil {
+				return err
 			}
 
-			if err := tf.Apply(doc); err != nil {
-				return errors.Wrapf(err, "failed to apply transformation %v", filenames[i])
+			for i, tf := range transformations {
+				ok, _ := tf.MustMatchAll(&doc)
+				if !ok {
+					continue
+				}
+
+				if err := tf.Apply(&doc); err != nil {
+					return errors.Wrapf(err, "failed to apply transformation %v", filenames[i])
+				}
 			}
-		}
 
-	case "grep":
-		selectors := os.Args[2:]
+		case "grep":
+			selectors := os.Args[2:]
 
-		// yaml grep -v "something"
-		invert := false
-		if selectors[0] == "-v" {
-			invert = true
-			selectors = selectors[1:]
-		}
-
-		tf, err := yaml.ParseTransformation([]byte(fmt.Sprintf("match:\n  %v", strings.Join(selectors, "\n  "))))
-		if err != nil {
-			return errors.Wrapf(err, "failed to parse grep `key: value' pairs from %q", selectors)
-		}
-
-		ok, _ := tf.MustMatchAll(doc)
-		if ok == invert {
-			return nil // Return; do not print anything out.
-		}
-
-		// Print out original doc.
-
-	case "delete":
-		selector := os.Args[2]
-
-		if err := yaml.Delete(doc, selector); err != nil {
-			if false { // TODO: --strict mode, where we'd error out on non-existent selectors?
-				return errors.Wrapf(err, "failed to delete %q", selector)
+			// yaml grep -v "something"
+			invert := false
+			if selectors[0] == "-v" {
+				invert = true
+				selectors = selectors[1:]
 			}
+
+			tf, err := yaml.ParseTransformation([]byte(fmt.Sprintf("match:\n  %v", strings.Join(selectors, "\n  "))))
+			if err != nil {
+				return errors.Wrapf(err, "failed to parse grep `key: value' pairs from %q", selectors)
+			}
+
+			ok, _ := tf.MustMatchAll(&doc)
+			if ok == invert {
+				continue // Do not print anything out.
+			}
+
+			// Print out original doc.
+
+		case "delete":
+			selector := os.Args[2]
+
+			if err := yaml.Delete(&doc, selector); err != nil {
+				if false { // TODO: --strict mode, where we'd error out on non-existent selectors?
+					return errors.Wrapf(err, "failed to delete %q", selector)
+				}
+			}
+
+		default:
+			return errors.Errorf("%v: unknown command", os.Args[1])
 		}
 
-	default:
-		return errors.Errorf("%v: unknown command", os.Args[1])
-	}
-
-	err = yaml.Write(os.Stdout, doc)
-	if err != nil {
-		return errors.Wrap(err, "failed to write to stdout")
+		if err := enc.Encode(&doc); err != nil {
+			return errors.Wrap(err, "failed to write to stdout")
+		}
 	}
 
 	return nil
