8d919a95562e4fd3a0f91255993cb02425bec08e
Vojtech Vitek <vojtech.vitek@gmail.com>
2019-11-07 12:35:57 +0100

    Add `yaml get --print-key' tests

--- pkg/cli/get_test.go
+++ pkg/cli/get_test.go
@@ -7,8 +7,6 @@ import (
 func TestGet(t *testing.T) {
 	var kubectlGetPod = readFile("_testfiles/kubectl-get-pod.yml")
 
-	// On single input object/document, get and print should behave the same.
-
 	tt := []*cliTestCase{
 		{
 			in:   kubectlGetPod,
@@ -38,6 +36,47 @@ func TestGet(t *testing.T) {
 		{
 			in:   kubectlGetPod,
 			args: []string{"get", "status.containerStatuses[2].state.terminated.finishedAt"},
+			err:  true,
+		},
+	}
+
+	for _, tc := range tt {
+		tc.runTest(t)
+	}
+}
+
+func TestGetPrintKey(t *testing.T) {
+	var kubectlGetPod = readFile("_testfiles/kubectl-get-pod.yml")
+
+	tt := []*cliTestCase{
+		{
+			in:   kubectlGetPod,
+			args: []string{"get", "--print-key", "status.containerStatuses[0].name"},
+			out:  "name: goose-metrixdb\n",
+		},
+		{
+			in:   kubectlGetPod,
+			args: []string{"get", "--print-key", "status.containerStatuses[0].state.terminated.finishedAt"},
+			out:  "finishedAt: \"2019-08-18T12:23:29Z\"\n",
+		},
+		{
+			in:   kubectlGetPod,
+			args: []string{"get", "--print-key", "status.containerStatuses[1].name"},
+			out:  "name: linkerd-proxy\n",
+		},
+		{
+			in:   kubectlGetPod,
+			args: []string{"get", "--print-key", "status.containerStatuses[1].state.running.startedAt"},
+			out:  "startedAt: \"2019-08-18T12:23:30Z\"\n",
+		},
+		{
+			in:   kubectlGetPod,
+			args: []string{"get", "--print-key", "status.containerStatuses[2].name"},
+			err:  true,
+		},
+		{
+			in:   kubectlGetPod,
+			args: []string{"get", "--print-key", "status.containerStatuses[2].state.terminated.finishedAt"},
 			err:  true,
 		},
 	}
