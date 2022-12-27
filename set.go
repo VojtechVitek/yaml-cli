4028c67a78dfed5768ad77c3ea7742cd2c726f77
李磊磊 <lill@bonree.com>
2022-12-27 17:59:35 +0800

    feat: make output literal

--- set.go
+++ set.go
@@ -7,6 +7,20 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+func literalNodes(node *yaml.Node) {
+	if node.Style == yaml.FlowStyle {
+		node.Style = yaml.LiteralStyle
+	}
+
+	for _, subNode := range node.Content {
+		if subNode.Style == yaml.FlowStyle {
+			subNode.Style = yaml.LiteralStyle
+		}
+
+		literalNodes(subNode)
+	}
+}
+
 func Set(doc *yaml.Node, path string, value *yaml.Node) error {
 	selectors := strings.Split(path, ".")
 
@@ -21,17 +35,20 @@ func Set(doc *yaml.Node, path string, value *yaml.Node) error {
 			*node = *value
 		} else if node.Kind == yaml.MappingNode && value.Kind == yaml.MappingNode {
 			// Append new values onto an existing map node.
-			node.Content = append(node.Content, value.Content...)
+			node.Content = append(value.Content, node.Content...)
 		} else if node.Kind == yaml.MappingNode && node.Content == nil {
 			// Overwrite a new map node we created in findNode(), as confirmed
 			// by the nil check (the node.Content wouldn't be nil otherwise).
 			*node = *value
 		} else if node.Kind == yaml.SequenceNode && value.Kind == yaml.SequenceNode {
 			// Append new values onto an existing array node.
-			node.Content = append(node.Content, value.Content...)
+			node.Content = append(value.Content, node.Content...)
 		} else {
 			return errors.Errorf("can't overwrite %v value (line: %v, column: %v) with %v value", node.Tag, node.Line, node.Column, value.Tag)
 		}
+
+		// always literal style
+		literalNodes(node)
 	}
 
 	return nil
